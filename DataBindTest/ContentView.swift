58dc381eb29c0b67cedef75e4425a12bc4ba66b3
PetrolMo <1850956@tongji.edu>
2020-11-12 14:53:09 +0800

    Initial Commit

---
+++ DataBindTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataBindTest
+//
+//  Created by 莫仪智 on 2020/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataBindTest/DataBindTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBindTestApp.swift
+//  DataBindTest
+//
+//  Created by 莫仪智 on 2020/11/12.
+//
+
+import SwiftUI
+
+@main
+struct DataBindTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
